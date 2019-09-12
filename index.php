563afe6de8240c2250a1c65476e83f5ec7182d29
miguel-mcvc <52935576+miguel-mcvc@users.noreply.github.com>
2019-09-12 17:59:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-require '/Meli/meli.php';
-$meli = new Meli('3606372242098310', 'kfSp2qpShUWLagiVzSTQClGwGM20PHnT');
 
-$redirectUrl = $meli->getAuthUrl("https://webcomm.herokuapp.com/test.php",Meli::$AUTH_URL['MLM']);
-
-if($_GET['code']){
 echo '$_GET['code']';
 else{
 echo 'pasale perrouuu <br/>';
