00f9f73ff45b434afdee0632bade379bd12c6b80
miguel-mcvc <52935576+miguel-mcvc@users.noreply.github.com>
2019-09-12 16:33:10 -0500

    Update configApp.php

--- configApp.php
+++ configApp.php
@@ -3,13 +3,13 @@
 /* Go to My Apps dashboard: https://developers.mercadolibre.com.ar/apps/home, and get the information you need in order to the following enviroment variables */
 
 /* Your Application Id */
-$appId = getenv('3606372242098310');
+$appId = getenv('App_ID');
 
 /* Your Secret Key */
-$secretKey = getenv('kfSp2qpShUWLagiVzSTQClGwGM20PHnT');
+$secretKey = getenv('Secret_Key');
 
 /* The Redirect url */
-$redirectURI = getenv('https://webcomm.herokuapp.com');
+$redirectURI = getenv('Redirect_URI');
 
 /* The site id of the country where your application will work.
 If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
