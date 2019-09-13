d7a2fba10e687ca7439cd1c75866999f20e7b9eb
miguel-mcvc <52935576+miguel-mcvc@users.noreply.github.com>
2019-09-12 22:26:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ require 'Meli/meli.php';
 
 $meli = new Meli('3606372242098310','kfSp2qpShUWLagiVzSTQClGwGM20PHnT');
 
-echo 'test 2';
+$redirectUrl = $meli->getAuthUrl("https://webcomm.herokuapp.com/auth.php", Meli::$AUTH_URL['MLM']);
+
+echo 'test 3';
 
 
 
 
 ?>
+<p><a href="<?=$redirectURL;?>">Link</a></p>
